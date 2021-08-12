db92857702cc0ffe0d6659b632f8b50d12d370b0
Zhengnan Zhao <zzhengnan@protonmail.com>
2021-08-11 23:10:21 -0400

    Add project skeleton

---
+++ README.md
@@ -0,0 +1,14 @@
+# `felix`
+
+A collection of handy decorators for Python.
+
+Named after [Allyson Felix](https://en.wikipedia.org/wiki/Allyson_Felix), the most _decorated_ woman in Olympic track and field history.
+
+# Installation
+
+```
+$ python -m pip install felix-decorator
+```
+
+# Resources
+The decorators in this package borrow heavily from this [excellent tutorial on Real Python](https://realpython.com/primer-on-python-decorators/).
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+
+import felix
+
+
+EXTRA_REQUIRES = {
+    'build': ['setuptools', 'twine', 'wheel'],
+    'qa': ['flake8'],
+    'test': ['mypy', 'pytest'],
+}
+
+EXTRA_REQUIRES['dev'] = (
+    EXTRA_REQUIRES['build'] +
+    EXTRA_REQUIRES['qa'] +
+    EXTRA_REQUIRES['test']
+)
+
+
+if __name__ == '__main__':
+    setup(
+        name='felix-decorator',
+        version=felix.__version__,
+        url='https://github.com/zzhengnan/felix',
+        author='Zhengnan Zhao',
+        description="A collection of handy decorators for Python",
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+        packages=find_packages(),
+        python_requires='>=3.7',
+        extra_requires=EXTRA_REQUIRES,
+        classifiers=[
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+        ]
+    )

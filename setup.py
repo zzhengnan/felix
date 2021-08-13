3a3fac068aaf8e74256a01d2528c6ee0e0c0ebfa
Zhengnan Zhao <zzhengnan@protonmail.com>
2021-08-12 21:37:36 -0400

    Remove main block

--- setup.py
+++ setup.py
@@ -16,23 +16,22 @@ EXTRA_REQUIRES['dev'] = (
 )
 
 
-if __name__ == '__main__':
-    setup(
-        name='felix-decorator',
-        version=felix.__version__,
-        url='https://github.com/zzhengnan/felix',
-        author='Zhengnan Zhao',
-        description="A collection of handy decorators for Python",
-        long_description=open('README.md').read(),
-        long_description_content_type='text/markdown',
-        packages=find_packages(),
-        python_requires='>=3.7',
-        extra_requires=EXTRA_REQUIRES,
-        classifiers=[
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: OS Independent',
-        ]
-    )
+setup(
+    name='felix-decorator',
+    version=felix.__version__,
+    url='https://github.com/zzhengnan/felix',
+    author='Zhengnan Zhao',
+    description="A collection of handy decorators for Python",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    python_requires='>=3.7',
+    extra_requires=EXTRA_REQUIRES,
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
